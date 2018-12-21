54e4c4d263aa7f39d1a03d504989c9421c102043
Chris Cowell <chris@trufflesuite.com>
2018-12-21 12:51:13 -0500

    Removed unused contracts

--- test/contracts/dynamicStrings/DynamicStringLengthCheck.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.2;
-
-contract DynamicStringLengthCheck {
-  function stringSettingExternally(string oldText, string newText) pure public {
-    require(keccak256(abi.encodePacked(oldText)) == keccak256(abi.encodePacked(newText)));
-  }
-}
